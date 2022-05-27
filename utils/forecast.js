49c33ba2c726de7cbba29331e48bc3b2c2c72130
abuYahya <abuyahyadiab@gmail.com>
2022-05-27 14:45:07 +0300

    A new update to forecast file

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,12 +9,16 @@ const forecast = (longitude,latitude,callback)=>{
         else if(body.error){
             callback("we can't handle the err, try agian!",undefined);
         }
-        else
+        else{
             callback(undefined,{
                 location: body.location.timezone_id,
                 temperature: body.current.temperature,
-                humidity: body.current.humidity
+                humidity: body.current.humidity,
+                wind_speed: body.current.wind_speed,
+                wind_dir: body.current.wind_dir
             });
+
+        }
     })
 }
 
